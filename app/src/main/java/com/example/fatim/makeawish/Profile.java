d76dda434fd755fbf2123f171151b192ab1f871d
fatimahkb <fatimah-kb@hotmail.com>
2019-02-16 23:34:46 +0300

    changes

--- app/src/main/java/com/example/fatim/makeawish/Profile.java
+++ app/src/main/java/com/example/fatim/makeawish/Profile.java
@@ -4,10 +4,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Profile extends AppCompatActivity {
+    int x =5+2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+    }
+    public void hello(){
+        int y = x+5;
+    }
+    public void bye(){
+        int y = x+5;
     }
 }
