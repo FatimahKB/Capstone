db369b07da0aab5cd618af90c1988b780a271ec2
fatimahkb <fatimah-kb@hotmail.com>
2019-03-09 12:35:11 +0300

    Hi haifa in profile.java

--- app/src/main/java/com/example/fatim/makeawish/Profile.java
+++ app/src/main/java/com/example/fatim/makeawish/Profile.java
@@ -12,6 +12,7 @@ public class Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        String blah= "Hi Haifa";
 
         Button btnPrivate = findViewById(R.id.profile_private_button);
         btnPrivate.setOnClickListener(new View.OnClickListener() {
