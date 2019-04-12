a230a1c6d0cb3bfa9f4c4cda2be51451a3998e8e
fatimahkb <fatimah-kb@hotmail.com>
2019-04-12 21:45:24 +0300

    -final merge

--- app/src/main/java/com/example/fatim/makeawish/SearchUserFragment.java
+++ app/src/main/java/com/example/fatim/makeawish/SearchUserFragment.java
@@ -5,10 +5,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class SearchUserFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup viewGroup, Bundle savedInstanceState){
-        return inflater.inflate(R.layout.search_users_layout, viewGroup, false);
+       // return inflater.inflate(R.layout.search_users_layout, viewGroup, false);
+        final View view = inflater.inflate(R.layout.search_users_layout, viewGroup, false);
+       Button btnSearchUser = (Button) view.findViewById(R.id.searchbtnUser);
+       btnSearchUser.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+
+           }
+       });
+
+        return view;
+
     }
 }
